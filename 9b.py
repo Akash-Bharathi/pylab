756f3489273603be6f6f9edce30f18bb8caab643
Akash-Bharathi <113248732+Akash-Bharathi@users.noreply.github.com>
2025-06-03 23:52:52 +0530

    Update 9b.py

--- 9b.py
+++ 9b.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = 'secret123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Models
@@ -54,6 +55,8 @@ def like(post_id):
     db.session.commit()
     return redirect('/feed')
 
+# Initialize DB and run app
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
